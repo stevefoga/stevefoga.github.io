2468ff7cfe7d66d455e29b6d5b12bb0a47fd5425
Steve Foga <steve.foga@gmail.com>
2022-07-07 22:01:36 -0400

    initial commit

---
+++ leaflet/scripts/chrome_test.sh
@@ -0,0 +1,2 @@
+# run chrome to allow CORS, external scripts (MacOS)
+open -n -a /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --args --user-data-dir="/tmp/chrome_dev_test" --disable-web-security
