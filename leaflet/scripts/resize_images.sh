b4756b4fa130dcd3847c7b186c49d0e58fcceaf8
Steve Foga <steve.foga@gmail.com>
2022-07-07 22:01:51 -0400

    space

--- leaflet/scripts/resize_images.sh
+++ leaflet/scripts/resize_images.sh
@@ -3,3 +3,6 @@ for f in *.*; do echo "$f"; convert "$f" -resize 20% -quality 70 -strip -auto-or
 
 # partial res -> reduced
 for f in *.*; do echo "$f"; convert "$f" -resize 50% -quality 90 -strip -auto-orient resize/"$f"; done
+
+# run chrome to allow CORS, external scripts (MacOS)
+open -n -a /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --args --user-data-dir="/tmp/chrome_dev_test" --disable-web-security
